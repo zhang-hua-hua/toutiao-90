8cb37df7e50247bce4fd7f93e73cd49c446daadf
unknown <fanyoufu@outlook.com>
2019-12-20 22:06:07 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,34 +3,34 @@
       <div class="title">
           <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-      <!-- 左侧导航文件 -->
-      <el-menu background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
+      <!-- 左侧导航文件  开启路由用 :router="true" 或 router-->
+      <el-menu router background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
           <!-- 一级菜单 -->
-          <el-menu-item>首页</el-menu-item>
+          <el-menu-item index="/home">首页</el-menu-item>
           <!-- 二级菜单 -->
-            <el-submenu>
-              <template slot="title">
-                    <span>内容管理</span>
-              </template>
+            <el-submenu index="1">
+
+                    <span slot="title">内容管理</span>
+
               <el-menu-item-group>
-              <el-menu-item index="1-1">发布文章</el-menu-item>
-              <el-menu-item index="1-2">文章列表</el-menu-item>
-              <el-menu-item index="1-3">评论管理</el-menu-item>
-              <el-menu-item index="1-3">素材管理</el-menu-item>
+              <el-menu-item index="/home/publish">发布文章</el-menu-item>
+              <el-menu-item index="/home/articles">文章列表</el-menu-item>
+              <el-menu-item index="/home/comment">评论管理</el-menu-item>
+              <el-menu-item index="/home/material">素材管理</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
-            <el-submenu>
-              <template slot="title">
-                    <span>粉丝管理</span>
-              </template>
+            <el-submenu index="2">
+
+                    <span slot="title">粉丝管理</span>
+
               <el-menu-item-group>
-              <el-menu-item index="1-4">图文数据</el-menu-item>
-              <el-menu-item index="1-5">粉丝概况</el-menu-item>
-              <el-menu-item index="1-6">粉丝画像</el-menu-item>
-              <el-menu-item index="1-6">粉丝列表</el-menu-item>
+              <el-menu-item index="/home/picture">图文数据</el-menu-item>
+              <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+              <el-menu-item index="/home/fanspicture">粉丝画像</el-menu-item>
+              <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
-          <el-menu-item>账户信息</el-menu-item>
+          <el-menu-item index="/home/userinfo">账户信息</el-menu-item>
       </el-menu>
   </div>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,10 @@
          <el-header>
            <layout-header></layout-header>
          </el-header>
-         <el-main>体</el-main>
+         <el-main>
+            <!-- 二级路由容器 -->
+          <router-view></router-view>
+         </el-main>
     </el-container>
   </el-container>
 </template>
