e9138864d1bbe9b43d37e8878905790ce40bd41e
unknown <fanyoufu@outlook.com>
2019-12-22 20:40:14 +0800

    评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-      评论列表
-  </div>
+  <!-- 卡片组件 -->
+  <el-card>
+      <!-- 组件面包屑 -->
+      <bread-crumb slot="header">
+        <!-- 插槽内容 -->
+        <template slot="title">
+            评论管理
+        </template>
+      </bread-crumb>
+      <!-- 表格 -->
+      <el-table>
+          <!-- 放置 -->
+        <el-table-column label="标题" width="800"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝品论数"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
