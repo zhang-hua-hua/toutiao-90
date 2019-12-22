8ef514b79003aca51c8d42854b9cafc8627bd9ed
unknown <fanyoufu@outlook.com>
2019-12-22 11:21:53 +0800

    PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- 面包屑组件 -->
+ <!-- 封装原来的el-breadcrumb -->
+ <el-breadcrumb separator=">">
+  <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- 定义一个具名插槽 -->
+      <slot name='title'></slot>
+  </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
