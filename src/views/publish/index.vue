a82b9fb61eb46698e9c7f4d52b88b72ae94476f9
unknown <fanyoufu@outlook.com>
2019-12-27 22:16:46 +0800

    发表文章-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -23,7 +23,9 @@
                </el-radio-group>
            </el-form-item>
            <el-form-item label="频道">
-               <el-select></el-select>
+               <el-select>
+                   <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
+               </el-select>
            </el-form-item>
            <el-form-item>
                <el-button type="primary">发布</el-button>
@@ -35,7 +37,24 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      channels: [] // 接收频道数据
+    }
+  },
+  methods: {
+    // 获取所有的频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
