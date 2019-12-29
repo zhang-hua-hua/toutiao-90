1aa820370ae79cd2d94b050e6dafa7b007c0aaa0
unknown <fanyoufu@outlook.com>
2019-12-29 17:41:43 +0800

    发表文章-修改文章-提交数据修改

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -98,9 +98,11 @@ export default {
     publishArticle (draft) {
       this.$refs.publishForm.validate(isOK => {
         if (isOK) {
+          // 判断是修改还是发布文章
+          let{ articleId } = this.$route.params // 获取动态路有参数
           this.$axios({
-            url: '/articles',
-            method: 'post',
+            url: articleId ? `/articles/${articleId}` : '/articles',
+            method: articleId ? 'put' : 'post',
             params: { draft }, // 查询参数
             data: this.formData// 请求体参数
           }).then(() => {
@@ -111,6 +113,36 @@ export default {
             // 跳转到文章列表页
             this.$router.push('/home/articles')
           })
+          // if (articleId) {
+          //   // 修改文章接口
+          //   this.$axios({
+          //     url: `/articles/${articleId}`,
+          //     params: { draft }, // 查询参数
+          //     data: this.formData// 请求体参数
+          //   }).then(result => {
+          //     this.$message({
+          //       type: 'success',
+          //       message: '保存成功'
+          //     })
+          //     this.$router.push('/home/articles')
+          //   })
+          // } else {
+          // 调用发布接口
+          // 调用发布接口
+          // this.$axios({
+          //   url: '/articles',
+          //   method: 'post',
+          //   params: { draft }, // 查询参数
+          //   data: this.formData// 请求体参数
+          // }).then(() => {
+          //   this.$message({
+          //     type: 'success',
+          //     message: '保存成功'
+          //   })
+          //   // 跳转到文章列表页
+          //   this.$router.push('/home/articles')
+          // })
+          // }
         }
       })
     },
@@ -125,7 +157,7 @@ export default {
   },
   created () {
     this.getChannels()
-    let{ articleId } = this.$route.params
+    let{ articleId } = this.$route.params // 获取动态路有参数
     articleId && this.getArticleById(articleId)// 如果文章id存在  直接查询文章的数据
   }
 }
